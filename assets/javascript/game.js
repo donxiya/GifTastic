9550ec2e4bd3d65d5e38b5fb2042e321476fc242
Htran_14 <hao.allen.tran@gmail.com>
2019-05-14 20:14:45 -0500

    fix bugs

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,39 +6,65 @@ var newBTN = function () {
     newBTN.attr("data-state", "default");
     newBTN.addClass("button");
     newBTN.text(newSUB);
+
     $("#buttonList").append(newBTN);
     $("#input").val("");
 };
 
 var searchGIF = function (term) {
+    
+
     var key = "c3jwjWyqTWqKljMuLXLRmxYXuZfrT7iD";
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + term + "&api_key=" + key;
+
     $.ajax({
         method: "GET",
         url: queryURL,
     }).then(function (response) {
+        if(response.data != null){
         var results = response.data;
         for (var i = 0; i < results.length; i++) {
             var gifBody = $("<img>");
             var gifDiv = $("<div>");
             var p = $("<div>");
-            gifBody.attr("src", results[i].images.fixed_height_still.url);
+            gifBody.attr("src", results[i].images.original_still.url);
             gifBody.attr("data-state", "still");
-            gifBody.attr("data-still", results[i].images.fixed_height_still.url);
-            gifBody.attr("data-animate", results[i].images.fixed_height.url);
+            gifBody.attr("data-still", results[i].images.original_still.url);
+            gifBody.attr("data-animate", results[i].images.original.url);
             gifBody.addClass("imgClick");
             gifDiv.addClass("imgContainer");
             p.addClass("text");
-            p.text(term);
+            p.text("Rating: "+response.data[i].rating);
             gifDiv.append(p);
             gifDiv.append(gifBody);
             $("#imagesList").append(gifDiv);
-        }
+            // $(".imgClick").click(function () {
+            //     var state = $(this).attr("data-state");
+            //     if (state === "still") {
+            //         console.log("animate");
+            //         $(this).attr("src", $(this).attr("data-animate"));
+            //         $(this).attr("data-state", "animate");
+            //     } else {
+            //         $(this).attr("src", $(this).attr("data-still"));
+            //         $(this).attr("data-state", "still");
+            //     }
+            // });
+        }};
+        $(".imgClick").click(function () {
+            var state = $(this).attr("data-state");
+            if (state === "still") {
+                $(this).attr("src", $(this).attr("data-animate"));
+                $(this).attr("data-state", "animate");
+            } else {
+                $(this).attr("src", $(this).attr("data-still"));
+                $(this).attr("data-state", "still");
+            }
+        });
     });
 };
 
-// $(".imgClick").click(function () {
-//     console.log("clicked img");
+
+// $(document).on('click', '.imgClick', function () {
 //     var state = $(this).attr("data-state");
 //     if (state === "still") {
 //         $(this).attr("src", $(this).attr("data-animate"));
@@ -48,16 +74,6 @@ var searchGIF = function (term) {
 //         $(this).attr("data-state", "still");
 //     }
 // });
-$(document).on('click', '.imgClick', function () {
-    var state = $(this).attr("data-state");
-    if (state === "still") {
-        $(this).attr("src", $(this).attr("data-animate"));
-        $(this).attr("data-state", "animate");
-    } else {
-        $(this).attr("src", $(this).attr("data-still"));
-        $(this).attr("data-state", "still");
-    }
-});
 $("#submit").on("click", function () {
     if ($("#input").val() != "") {
         newBTN();
@@ -65,7 +81,6 @@ $("#submit").on("click", function () {
 });
 $(document).on('click', ".button, .buttonClicked", function () {
     $("#imagesList").empty();
-    console.log("default");
     $(".buttonClicked").removeClass("buttonClicked").addClass("button");
     $(this).addClass("buttonClicked");
     $(this).removeClass("button");
